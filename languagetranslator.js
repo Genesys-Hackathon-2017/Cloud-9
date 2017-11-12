279491e72e1ca6ae2dff5a1d7f15c6c96fc04918
Vimal-Raghubir <vraghubir0418@gmail.com>
2017-11-12 05:24:27 -0500

    Added language translator file

---
+++ languagetranslator.js
@@ -0,0 +1,38 @@
+// Required node packages for our application
+var http = require("http");
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
+
+var app = express();
+var sessionMap = {};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({ extended: true}));
+app.use(express.static(__dirname));
+
+var language_translator = new LanguageTranslatorV2({
+  username: '55964c59-cd74-4963-abdd-4b36a9eca912',
+  password: 'cGBxOrDKleGg',
+  url: 'https://gateway.watsonplatform.net/language-translator/api/'
+});
+
+function testFunction(input) {
+language_translator.translate({
+  text: input, source : 'en', target: 'fr' },
+  function (err, result) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(result.translations[0].translation);
+});
+}
+
+//var prompt = require('prompt-sync')();
+
+app.post("/readMsg", function(req, res){
+    testFunction(req.body.message);
+});
+
+var httpServer = http.createServer(app);
+httpServer.listen('3000');
